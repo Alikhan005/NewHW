3119590743046db99c9cc68ec40c2180e3f4a8e7
ALIKHAN SAPAR <saparov.05@mail.ru>
2025-01-28 10:27:08 +0600

    Cleaned up unnecessary files

--- Desktop/hw_2/hw_2/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.get_articles, name='get_articles'),
-    path('<int:id>/', views.get_article_by_id, name='get_article_by_id'),
-]
